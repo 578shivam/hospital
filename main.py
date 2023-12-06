c66defccd79e281060d874c166cfd9ba039f2d2d
578shivam <17shivamkumar2004@gmail.com>
2023-12-06 21:28:11 +0530

    hellow shivam

---
+++ main.py
@@ -0,0 +1,14 @@
+#def nubmer(x,y):
+#  sum=x+y
+# return sum
+#add=20
+#add1=30
+#print("the defined user:",add+add1)
+
+x = 5
+def funct():
+    x = 10
+    print("local x:", x)
+
+funct()
+print("global x:", x)
